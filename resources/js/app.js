99dcab5883f7154a7b77e013273b7bf729d375f0
Motionphix <kingscrib1@gmail.com>
2023-02-25 07:36:37 +0200

    initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,23 @@
+import "./bootstrap";
+import { install } from '@twind/core'
+import "@protonemedia/laravel-splade/dist/style.css";
+
+import { createApp } from "vue/dist/vue.esm-bundler.js";
+import { renderSpladeApp, SpladePlugin } from "@protonemedia/laravel-splade";
+
+import config from '../../twind.config'
+
+const el = document.getElementById("app");
+
+// activate twind - must be called at least once
+install(config)
+
+createApp({
+    render: renderSpladeApp({ el })
+})
+    .use(SpladePlugin, {
+        "max_keep_alive": 10,
+        "transform_anchors": false,
+        "progress_bar": true
+    })
+    .mount(el);
---
+++ twind.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from '@twind/core'
+import presetTailwind from '@twind/preset-tailwind'
+import presetAutoprefix from '@twind/preset-autoprefix'
+import presetTailwindForms from '@twind/preset-tailwind-forms'
+import pressetContainerQueries from '@twind/preset-container-queries'
+
+export default defineConfig({
+  presets: [
+    presetAutoprefix(),
+    presetTailwind(),
+    presetTailwindForms(),
+    pressetContainerQueries(),
+  ],
+
+  darkMode: 'class',
+
+  rules: [
+    ['btn', 'bg-gray-800 text-gray-300 dark:text-gray-400 rounded font-semibold px-2 py-1 transition duration-300 dark:bg-gray-800 hover:(dark:text-gray-500 bg-gray-700)'],
+  ],
+})
