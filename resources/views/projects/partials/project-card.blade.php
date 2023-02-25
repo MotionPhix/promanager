56eb0969f5cd8f4f112e3597e947982b8f989723
Motionphix <kingscrib1@gmail.com>
2023-02-25 12:45:16 +0200

    added `partials` to projects

---
+++ resources/views/projects/partials/project-card.blade.php
@@ -0,0 +1,10 @@
+<div class="bg-white rounded-lg shadow-md hover:shadow-lg transition duration-300">
+  <div class="p-4">
+    <h3 class="text-lg font-medium text-gray-900">Project A</h3>
+    <p class="text-sm text-gray-500 mb-2">Deadline: Jan 30, 2022</p>
+    <p class="text-sm text-gray-500 mb-2">Budget: $50,000</p>
+    <p class="text-sm text-gray-500 mb-2">Assigned to: John Doe, Jane Smith, Alex Kim</p>
+    <p class="text-sm text-gray-500 mb-2">Status: In Progress</p>
+    <a href="#" class="text-sm text-gray-700 hover:text-blue-500">View Project</a>
+  </div>
+</div>
